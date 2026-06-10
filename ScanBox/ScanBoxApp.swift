37d1c02900140b142bd32a8fda15f054701af627
Victor Augusto Borghi <victoraugustotlb@gmail.com>
2026-06-10 18:10:54 -0300

    Add files via upload

---
+++ ScanBox/ScanBoxApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScanBoxApp.swift
+//  ScanBox
+//
+//  Created by Victor Augusto Toledo Lúcio Borghi on 10/06/26.
+//
+
+import SwiftUI
+
+@main
+struct ScanBoxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
